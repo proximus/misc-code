c0d7fd4963e67043f0dc5a11de66264baa0dde24
Samuel Gabrielsson <samuel.gabrielsson@gmail.com>
2013-02-27 02:34:08 +0100

    git-daily-sync.sh: Update with variables

--- git-daily-sync/git-daily-sync.sh
+++ git-daily-sync/git-daily-sync.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 #===============================================================================
 #
 #          FILE:  git-daily-sync.sh
@@ -27,13 +27,15 @@
 function print_usage()
 {
     local prog_name=$(basename "$0")
-    echo "$prog_name is a program to do daily integation and sync from a git repository"
-    echo "Usage: $prog_name -b <teambranch> [-h]
+    echo "Usage: $prog_name -b <teambranch> [-h] [-d]
+
+$prog_name is a program to do daily integation and sync from a git repository
 
 Options:
     -b <teambranch>     where <team-branch> is in the format <branch>-<type>-<team>. For example:
-                        master-int-board, r48ya-int-board, master-int-tr, r48ya-int-tr.
+                        master-int-board, r48ya-int-board, master-int-tr or r48ya-int-tr.
     -h                  show this help text.
+    -d                  show debug information.
 
 Report bugs to samuel.gabrielsson@gmail.com
 Integrator home page: <http://ki81fw4.rnd.ki.sw.ericsson.se/tiki/tiki-index.php?page=RA_Radio_Git_Integration>" >&2
@@ -46,6 +48,8 @@ Integrator home page: <http://ki81fw4.rnd.ki.sw.ericsson.se/tiki/tiki-index.php?
 #===============================================================================
 function parse_opts()
 {
+    local prog_name=$(basename "$0")
+
     while getopts :b:dh arg; do
         case $arg in
         b)
@@ -58,17 +62,17 @@ function parse_opts()
             ;;
         h)
             print_usage
-            exit
+            return 1 # TODO: Should return 0 but exit doesnt work correct
             ;;
         \?)
-            echo "Error: Invalid option: -$OPTARG" >&2
-            print_usage
-            exit 1
+            echo "$prog_name: invalid option - '$OPTARG'" >&2
+            echo "Try \`$prog_name -h' for more information." >&2
+            return 1
             ;;
         :)
-            echo "Error: Option -$OPTARG requires an argument." >&2
-            print_usage
-            exit 1
+            echo "$prog_name: option - '$OPTARG' requires an argument" >&2
+            echo "Try \`$prog_name -h' for more information." >&2
+            return 1
             ;;
         esac
     done
@@ -78,14 +82,14 @@ function parse_opts()
 #===============================================================================
 # Split the team branch array into three variables
 # Usage: get_name
-# TODO
+# Return: branch_name, branch_type, branch_team
 #===============================================================================
 function get_name()
 {
     local IFS=$'\n'
     local arr
     local line
-    local git_branch
+    local remote_team_branch
 
     for line in $(echo $1); do
         IFS='-'
@@ -94,49 +98,55 @@ function get_name()
         branch_type=${arr[1]}
         branch_team=${arr[2]}
     done
+
     # Check if names are not empty
     if [ -z "$branch_name" ] || [ -z "$branch_type" ] || [ -z "$branch_team" ]; then
         echo "Error: teambranch does not have the correct format. Should be <branch>-<type>-<team>"
-        print_usage
-        exit 1
+        return 1
     fi
-    # Check if branch name is in remote
-    #git_branch=$(git branch --no-color -r | grep "^[ ]+origin/$branch_name-$branch_type-$branch_team" | tr -d ' ')
-    git_branch=$(git branch --no-color -r | grep '^[ ]\+origin/master$' | tr -d ' ')
-    #if [ -n "$git_branch" ] && [ -n ]
+
+    # Check if team branch exists in remote
+    remote_team_branch=$(git branch --no-color -r | grep "^[ ]+origin/$branch_name-$branch_type-$branch_team" | tr -d ' ')
+    if [ -z "$remote_team_branch" ]; then
+        echo "Error: Remote branch origin/$1 does not exist"
+        return 1
+    fi
+
+    # Check if branch has right format
+
+    # Check if type has right format
+    return 0
 }
 
+# TODO: Fix this
 if [ "$#" -eq 0 ]; then
     print_usage
     exit 1
 fi
 
-get_name $(parse_opts "$@")
-echo $branch_name
-echo $branch_type
-echo $branch_team
+options=$(parse_opts "$@") || exit
+get_name "$options" || exit
+#get_name $(parse_opts "$@")
+echo "branch_name: $branch_name"
+echo "branch_type: $branch_type"
+echo "branch_team: $branch_team"
 
-exit 1
+exit 0
 # Set variables
-set MY_TEAM_BRANCH="master-int-board"
+if [ "${branch_name}" == "master" ]; then
+    set MY_TEAM_BRANCH="master-int-board"
 
-set MY_DELIVERY_BRANCH="master"
-set MY_BASELINE_BRANCH="master"
-set GIT_CC_BRANCH="cc-main"
+    set MY_DELIVERY_BRANCH="master"
+    set MY_BASELINE_BRANCH="master"
+    set GIT_CC_BRANCH="cc-main"
 
-set GIT_PRIMAL_BRANCH="${MY_DELIVERY_BRANCH}"
-set GIT_INTEGRATION_BRANCH="${MY_TEAM_BRANCH}"
-set GIT_BASELINE_BRANCH="${MY_BASELINE_BRANCH}"
-set CLEARCASE_REFERENCE_VIEW="${USER}_${MY_DELIVERY_BRANCH}_reference"
-set CLEARCASE_DELIVERY_VIEW="${USER}_${MY_DELIVERY_BRANCH}_delivery"
+else
+    set MY_TEAM_BRANCH="r48ya-int-board"
 
-
-set MY_TEAM_BRANCH="r48ya-int-board"
-
-set MY_DELIVERY_BRANCH="r48ya"
-set MY_BASELINE_BRANCH="v10.6.0-r48ya"
-set GIT_CC_BRANCH="cc-${MY_DELIVERY_BRANCH}"
-
+    set MY_DELIVERY_BRANCH="r48ya"
+    set MY_BASELINE_BRANCH="v10.6.0-r48ya"
+    set GIT_CC_BRANCH="cc-${MY_DELIVERY_BRANCH}"
+fi
 set GIT_PRIMAL_BRANCH="${MY_DELIVERY_BRANCH}"
 set GIT_INTEGRATION_BRANCH="${MY_TEAM_BRANCH}"
 set GIT_BASELINE_BRANCH="${MY_BASELINE_BRANCH}"
